e40eb95aa1c76d5d40022b99e9f9d4d63ebe46f4
i1111111 <s1101702@gm.pu.edu.tw>
2022-12-01 20:26:55 +0800

    make it better

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, jsonify
 from datetime import datetime, timezone, timedelta
 import firebase_admin
 from firebase_admin import credentials, firestore
@@ -69,6 +69,17 @@ def search():
         return result
     else:
         result = "抱歉，查無相關條件的選修課程"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    action =  req.get("queryResult").get("action")
+    msg =  req.get("queryResult").get("queryText")
+    info = "動作：" + action + "； 查詢內容：" + msg
+    return make_response(jsonify({"fulfillmentText": info}))
+
 
 if __name__ == "__main__":
     app.run()
